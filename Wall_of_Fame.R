af3995e4b71ef7c49a7ea0999b5b750187400e85
Jason Wilson <jason.wilson@biola.edu>
2024-04-02 14:39:49 -0700

    Initial file post

---
+++ Wall_of_Fame.R
@@ -0,0 +1,5 @@
+# Now that you've pulled this script down, please add your name to the bottom
+# of your class list, and then push it back up to the repository!
+
+##### Spring 2024 Class List #####
+# 1. Dr. Wilson
